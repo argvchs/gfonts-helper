232fbef5a614d24527080985a0fe1b5cb6e1ac84
argvchs <argvchs@outlook.com>
2022-08-23 10:11:31 +0800

    ...

--- index.test.js
+++ index.test.js
@@ -8,7 +8,7 @@ var url = process.argv[2].replace(/http:/, "https:");
 const options = {
     headers: {
         "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
     },
 };
 function download(url, dest) {
