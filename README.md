a6b4e53168c8528e12b938b8884554e2ed0cd572
argvchs <argvchs@outlook.com>
2023-01-03 22:52:53 +0800

    add group argument

--- README.md
+++ README.md
@@ -11,19 +11,25 @@ npm i -g gfonts-helper
 ## 2. 使用
 
 ```bash
-gfonts-helper <url>
+gfonts-helper <url> <group>
 ```
 
-`<url>` 是 Google Fonts API 的网址，e.g.
+`<url>` 是 Google Fonts API 的 URL
+
+`<group>` 是分组下载的每组数量，因为一次下载可能最后会卡住，默认为 20
+
+e.g.
 
 ```bash
 gfonts-helper "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+gfonts-helper "https://fonts.loli.net/css2?family=Roboto&display=swap"
+gfonts-helper "https://fonts.geekzu.org/css2?family=Roboto&display=swap"
 ```
 
 还可以换成缩写
 
 ```bash
-gfhelper <url>
+gfhelper <url> <group>
 ```
 
 运行完生成 `download.css` `fonts.min.css` 两个文件和 `fonts` 一个文件夹
--- index.js
+++ index.js
@@ -1,30 +1,66 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const CleanCSS = require("clean-css");
-const download = require("download");
-const regm = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
-const regr = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+\//g;
-var url = process.argv[2];
+const https = require("https");
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+const FILE = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
+const DOMAIN = /(https?|ftp|file):\/\/[-\w+&@#%=~|?!:,.;]+/g;
+let url = process.argv[2].replace(/http:/, "https:");
+let group = Number(process.argv[3]) || 20;
 const options = {
     headers: {
         "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
+            "Mozilla/5.0 (Windows NT group.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
     },
 };
+function getdir(url) {
+    return url.split("/").slice(0, -1).join("/");
+}
+function download(url, path) {
+    return new Promise(cb => {
+        https.get(url, options, res => {
+            let dir = getdir(path);
+            if (dir)
+                try {
+                    fs.accessSync(dir);
+                } catch {
+                    fs.mkdirSync(dir, { recursive: true });
+                }
+            let writeStream = fs.createWriteStream(path);
+            res.pipe(writeStream);
+            writeStream.on("finish", () => {
+                writeStream.close();
+                cb();
+            });
+            writeStream.on("error", err => {
+                writeStream.close();
+                console.error(err);
+            });
+        });
+    });
+}
 async function main() {
-    fs.writeFileSync("download.css", await download(url, undefined, options));
+    await download(url, "download.css");
     fs.readFile("download.css", "utf-8", async (err, data) => {
         if (err) {
             console.error(err);
             return;
         }
-        let files = data.match(regm);
-        await Promise.all(files.map((url) => download(url, "fonts")));
-        let file = data.replace(regr, "fonts/");
-        let minified = new CleanCSS().minify(file).styles;
-        fs.writeFile("fonts.min.css", minified, (err) => {
+        let fonts = data.match(FILE);
+        let count = 0;
+        for (let i = 0; i < fonts.length; i += group) {
+            let slice = fonts.slice(i, i + group);
+            await Promise.all(slice.map(url => download(url, url.replace(DOMAIN, "fonts"), fonts)));
+            count += group;
+            console.log(count, fonts.length);
+            await sleep(100);
+        }
+        let style = data.replace(DOMAIN, "fonts");
+        let minified = new CleanCSS().minify(style).styles;
+        fs.writeFile("fonts.min.css", minified, err => {
             if (err) console.error(err);
         });
     });
 }
+
 main();
