26de9f74b1ea95c9ab3469945252b6e5b04b8bc7
argvchs <argvchs@outlook.com>
2023-02-03 14:16:42 +0800

    use unary + operator instead of parseint & format

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 #!/usr/bin/env node
+
 const fs = require("fs");
 const CleanCSS = require("clean-css");
 const https = require("https");
-const sleep = ms => new Promise(res => setTimeout(res, ms));
+
 const FILE = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
 const DOMAIN = /(https?|ftp|file):\/\/[-\w+&@#%=~|?!:,.;]+/g;
-let url = process.argv[2].replace(/http:/, "https:");
-let group = parseInt(process.argv[3]) || 20;
-const options = {
+const OPTIONS = {
     headers: {
         "User-Agent":
-            "Mozilla/5.0 (Windows NT group.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     },
 };
-function getdir(url) {
+
+let url = process.argv[2].replace(/http:/, "https:");
+let group = +process.argv[3] || 20;
+
+function getdirectory(url) {
     return url.split("/").slice(0, -1).join("/");
 }
 function download(url, path) {
-    return new Promise(cb => {
-        https.get(url, options, res => {
-            let dir = getdir(path);
+    return new Promise(resolve => {
+        https.get(url, OPTIONS, res => {
+            let dir = getdirectory(path);
             if (dir)
                 try {
                     fs.accessSync(dir);
                 } catch {
                     fs.mkdirSync(dir, { recursive: true });
                 }
-            let writeStream = fs.createWriteStream(path);
-            res.pipe(writeStream);
-            writeStream.on("finish", () => {
-                writeStream.close();
-                cb();
+            let ws = fs.createWriteStream(path);
+            res.pipe(ws);
+            ws.on("finish", () => {
+                ws.close();
+                resolve();
             });
-            writeStream.on("error", err => {
-                writeStream.close();
-                console.error(err);
+            ws.on("error", err => {
+                ws.close();
+                throw err;
             });
         });
     });
 }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function main() {
     await download(url, "download.css");
     fs.readFile("download.css", "utf-8", async (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+        if (err) throw err;
         let fonts = data.match(FILE);
         let count = 0;
         for (let i = 0; i < fonts.length; i += group) {
@@ -58,7 +62,7 @@ async function main() {
         let style = data.replace(DOMAIN, "fonts");
         let minified = new CleanCSS().minify(style).styles;
         fs.writeFile("fonts.min.css", minified, err => {
-            if (err) console.error(err);
+            if (err) throw err;
         });
     });
 }
