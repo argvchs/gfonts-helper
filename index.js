c5b8f6da00cf8365e863573a796f97d19bb47ad5
argvchs <argvchs@outlook.com>
2023-05-05 18:42:53 +0800

    refactor: format code

--- index.js
+++ index.js
@@ -4,33 +4,27 @@ const fs = require("fs");
 const path = require("path");
 const CleanCSS = require("clean-css");
 const https = require("https");
-
-const URL = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
-const DOMAIN = /(https?|ftp|file):\/\/[-\w+&@#%=~|?!:,.;]+/g;
-const OPTIONS = {
+const reFile = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
+const reDomain = /(https?|ftp|file):\/\/[-\w+&@#%=~|?!:,.;]+/g;
+const options = {
     headers: {
         "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     },
 };
-
-let url = process.argv[2].replace(/http:/, "https:");
-let group = +process.argv[3] || 20;
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
 }
-
-function download(url, file) {
+function download(source, dest) {
     return new Promise((resolve, reject) => {
-        https.get(url, OPTIONS, res => {
-            let dir = path.dirname(file);
+        https.get(source, options, res => {
+            let dir = path.dirname(dest);
             try {
                 fs.accessSync(dir);
             } catch {
                 fs.mkdirSync(dir, { recursive: true });
             }
-            let ws = fs.createWriteStream(file);
+            let ws = fs.createWriteStream(dest);
             res.pipe(ws);
             ws.on("finish", () => {
                 ws.close();
@@ -43,22 +37,27 @@ function download(url, file) {
         });
     });
 }
-
 async function main() {
-    await download(url, "download.css");
-    let css = fs.readFileSync("download.css").toString();
-    let urls = css.match(URL);
-    for (let i = 0; i < urls.length; i += group) {
-        console.log(i, "/", urls.length);
+    let source, group;
+    try {
+        source = process.argv[2].replace(/http:/, "https:");
+        group = +process.argv[3] || 20;
+    } catch {
+        return;
+    }
+    await download(source, "download.css");
+    let style = fs.readFileSync("download.css").toString();
+    let files = style.match(reFile);
+    for (let i = 0; i < files.length; i += group) {
+        console.log(i, "/", files.length);
         let promises = [];
-        for (let url of urls.slice(i, i + group))
-            promises.push(download(url, url.replace(DOMAIN, "fonts")));
+        for (let file of files.slice(i, i + group))
+            promises.push(download(file, file.replace(reDomain, "fonts")));
         await Promise.all(promises);
         await sleep(100);
     }
-    let min = new CleanCSS().minify(css.replace(DOMAIN, "fonts")).styles;
-    fs.writeFileSync("fonts.min.css", min);
+    let minify = new CleanCSS().minify(style.replace(reDomain, "fonts")).styles;
+    fs.writeFileSync("fonts.min.css", minify);
     fs.unlinkSync("download.css");
 }
-
 main();
