13e55243aee88b200eea50829b8878a37e7edcda
argvchs <argvchs@outlook.com>
2023-09-18 23:15:40 +0800

    style: format code

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ const fs = require("fs");
 const path = require("path");
 const CleanCSS = require("clean-css");
 const https = require("https");
-const reFile = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
-const reDomain = /(https?|ftp|file):\/\/[-\w+&@#%=~|?!:,.;]+/g;
+const reg1 = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
+const reg2 = /(https?|ftp|file):\/\/[-\w+&@#%=~|?!:,.;]+/g;
 const options = {
     headers: {
         "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     },
 };
-function sleep(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
-function download(source, dest) {
-    return new Promise((resolve, reject) => {
+const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
+const download = (source, dest) =>
+    new Promise((resolve, reject) => {
         https.get(source, options, ret => {
             let dir = path.dirname(dest);
             try {
@@ -36,8 +34,7 @@ function download(source, dest) {
             });
         });
     });
-}
-async function main() {
+(async () => {
     let source, group;
     try {
         source = process.argv[2].replace(/http:/, "https:");
@@ -47,17 +44,15 @@ async function main() {
     }
     await download(source, "download.css");
     let style = fs.readFileSync("download.css").toString();
-    let files = style.match(reFile);
+    let files = style.match(reg1);
     for (let i = 0; i < files.length; i += group) {
         console.log(i, "/", files.length);
         let promises = [];
-        for (let file of files.slice(i, i + group))
-            promises.push(download(file, file.replace(reDomain, "fonts")));
+        for (let file of files.slice(i, i + group)) promises.push(download(file, file.replace(reg2, "fonts")));
         await Promise.all(promises);
         await sleep(100);
     }
-    let minify = new CleanCSS().minify(style.replace(reDomain, "fonts")).styles;
+    let minify = new CleanCSS().minify(style.replace(reg2, "fonts")).styles;
     fs.writeFileSync("fonts.min.css", minify);
     fs.unlinkSync("download.css");
-}
-main();
+})();
