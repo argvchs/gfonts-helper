efd0d90d09817f2cd576770ab94a5730acb63ee2
argvchs <argvchs@outlook.com>
2024-04-21 20:17:11 +0800

    refactor!: migrate to ESM
    docs: update docs
    
    chore: bump version

--- README.md
+++ README.md
@@ -11,19 +11,19 @@ pnpm add gfonts-helper -g
 # 2. 使用
 
 ```bash
-gfonts-helper <source> <group>
+gfonts-helper <source> <split>
 ```
 
-`<source>` 是一个 Google Fonts 的链接。
+`<source>` 是一个 Google Fonts API 的链接。
 
-`<group>` 是分组下载的每组数量，因为一次下载可能最后会卡住，默认为 20。
+`<split>` 是分组下载的每组数量，因为全部下载可能最后会卡住，默认为 20。
 
 e.g.
 
 ```bash
-gfonts-helper "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-gfonts-helper "https://fonts.loli.net/css2?family=Roboto&display=swap"
-gfonts-helper "https://fonts.geekzu.org/css2?family=Roboto&display=swap"
+gfonts-helper 'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@100..900&display=swap'
+gfonts-helper 'https://fonts.loli.net/css2?family=Noto+Sans+SC:wght@100..900&display=swap'
+gfonts-helper 'https://fonts.geekzu.org/css2?family=Noto+Sans+SC:wght@100..900&display=swap'
 ```
 
-运行完生成 `fonts.min.css` 一个文件，和 `fonts` 一个文件夹。
+运行完会生成 `fonts.min.css` 样式文件，和一个文件夹 `fonts/` 存储字体文件。
--- index.js
+++ index.js
@@ -1,58 +1,33 @@
-#!/usr/bin/env node
-
-const fs = require("fs");
-const path = require("path");
-const CleanCSS = require("clean-css");
-const https = require("https");
-const reg1 = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
-const reg2 = /(https?|ftp|file):\/\/[-\w+&@#%=~|?!:,.;]+/g;
-const options = {
-    headers: {
-        "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-    },
-};
-const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
-const download = (source, dest) =>
-    new Promise((resolve, reject) => {
-        https.get(source, options, ret => {
-            let dir = path.dirname(dest);
-            try {
-                fs.accessSync(dir);
-            } catch {
-                fs.mkdirSync(dir, { recursive: true });
-            }
-            let ws = fs.createWriteStream(dest);
-            ret.pipe(ws);
-            ws.on("finish", () => {
-                ws.close();
-                resolve();
-            });
-            ws.on("error", err => {
-                ws.close();
-                reject(err);
-            });
-        });
-    });
-(async () => {
-    let source, group;
-    try {
-        source = process.argv[2].replace(/http:/, "https:");
-        group = +process.argv[3] || 20;
-    } catch {
-        return;
-    }
-    await download(source, "download.css");
-    let style = fs.readFileSync("download.css").toString();
-    let files = style.match(reg1);
-    for (let i = 0; i < files.length; i += group) {
-        console.log(i, "/", files.length);
-        let promises = [];
-        for (let file of files.slice(i, i + group)) promises.push(download(file, file.replace(reg2, "fonts")));
-        await Promise.all(promises);
-        await sleep(100);
-    }
-    let minify = new CleanCSS().minify(style.replace(reg2, "fonts")).styles;
-    fs.writeFileSync("fonts.min.css", minify);
-    fs.unlinkSync("download.css");
-})();
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as stream from "node:stream";
+import CleanCSS from "clean-css";
+let reg1 = /(https?|ftp|file):\/\/[-\w+&@#/%=~|?!:,.;]+[-\w+&@#/%=~|]/g;
+let reg2 = /(https?|ftp|file):\/\/[-\w+&@#%=~|?!:,.;]+/g;
+let useragent =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36";
+let options = { headers: { "User-Agent": useragent } };
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+async function download(url, file) {
+    let dir = path.dirname(file);
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+    let response = await fetch(url, options);
+    await stream.promises.finished(
+        stream.Readable.fromWeb(response.body).pipe(fs.createWriteStream(file))
+    );
+}
+let url = process.argv[2];
+let split = parseInt(process.argv[3]) || 20;
+await download(url, "cache.css");
+let style = fs.readFileSync("cache.css").toString();
+let urls = style.match(reg1);
+for (let i = 0; i < urls.length; i += split) {
+    console.log(i, "/", urls.length, "(" + (i / urls.length * 100).toFixed(2) + "%)");
+    let slice = urls.slice(i, i + split);
+    await Promise.all(slice.map((url) => download(url, url.replace(reg2, "fonts"))));
+    await sleep(100);
+}
+console.log(urls.length, "/", urls.length, "(100.00%)");
+let minified = new CleanCSS().minify(style.replace(reg2, "fonts")).styles;
+fs.writeFileSync("fonts.min.css", minified);
+fs.unlinkSync("cache.css");
